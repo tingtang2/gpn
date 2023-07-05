d7458f6c37ed3093ae98c3dcc26589592a44136d
Ting Chen <zc2666@columbia.edu>
2023-07-05 09:19:53 -0400

    fix config passing

--- gpn/run_mlm.py
+++ gpn/run_mlm.py
@@ -396,7 +396,7 @@ def main():
         config = AutoConfig.from_pretrained(model_args.model_name_or_path,
                                             **config_kwargs)
     else:
-        config = CONFIG_MAPPING[model_args.model_type]()
+        config = CONFIG_MAPPING[model_args.model_type](**config_kwargs)
         logger.warning(
             "You are instantiating a new config instance from scratch.")
         if model_args.config_overrides is not None:
